24310aef1569c550a062e56442c10f1946018695
KardaElena <karda.elena@gmail.com>
2022-10-14 17:25:39 +0300

    Added Task about even numbers

---
+++ Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿// See Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+
+Console.WriteLine("В этой программе вы сможете получить все четные числа от 1 до вашего числа :) Попробуйте прямо сейчас!");
+
+Console.Write("\nВведите ваше число (чур не менее 2): ");
+
+int number = int.Parse(Console.ReadLine());
+
+int even = 2;
+
+if (number < even)
+{
+    Console.WriteLine($"\n Вы ввели число {number}, которое меньше 2. Невозможно составить промежуток");
+}
+
+else
+{  
+    while (even <= number)
+    {
+        if (even == 2)
+        {
+            Console.Write($"\n2");
+        }
+        else
+        {
+        Console.Write($", {even}");
+        }
+    even = even+2;
+    } 
+    Console.WriteLine(".");
+    Console.WriteLine ($"\nЭто все числа от 1 до {number}");
+    Console.WriteLine ("\n");
+}
