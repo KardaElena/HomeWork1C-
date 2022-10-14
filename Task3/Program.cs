e37339f158a650236b45b6508551d15f97a7d676
KardaElena <karda.elena@gmail.com>
2022-10-14 16:37:39 +0300

    Added task about even and odd

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Clear();
+Console.WriteLine("В этой программе вы сможете определить, является ли число четным.\t");
+
+Console.Write("\nВведите ваше число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+int rem = number%2;
+
+if (rem == 0)
+{
+    Console.WriteLine("\nЭто число четное\t");
+}
+else
+{
+    Console.WriteLine("\nЭто число нечетное");
+}
+
+
